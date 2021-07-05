62f18bfbc9c609b9daa876696793734661ce5769
Allan Hardy <al_hardy@live.com.au>
2021-07-05 21:51:53 +1000

    adding ecr iam policies

--- hub-ecr/local.auto.tfvars
+++ hub-ecr/local.auto.tfvars
@@ -1,3 +1,3 @@
 aws_region      = "ap-southeast-2"
 aws_account_id  = "963600901337"
-aws_assume_role = "DeveloperAccessRole"
+aws_assume_role = "TerraformAccessRole"
--- hub-ecr/main.tf
+++ hub-ecr/main.tf
@@ -1,5 +1,15 @@
+data "aws_iam_role" "ecr_full_access" {
+  name = "EcrFullAccessRole"
+}
+
+data "aws_iam_role" "ecr_readonly_readonly" {
+  name = "EcrReadonlyAccessRole"
+}
+
 module "ecr" {
-  source  = "cloudposse/ecr/aws"
-  version = "0.32.2"
-  namespace = "alhardynet"
+  source                     = "cloudposse/ecr/aws"
+  version                    = "0.32.2"
+  namespace                  = "alhardynet"
+  principals_full_access     = [data.aws_iam_role.ecr_full_access.arn]
+  principals_readonly_access = [data.aws_iam_role.ecr_readonly_readonly.arn]
 }
