b59f71f463203440b9101ba3c12c9b639c13ae81
Allan Hardy <al_hardy@live.com.au>
2021-07-12 19:22:32 +1000

    re-enable dns cert validation, was an issue with dns

--- ecs/domain.tf
+++ ecs/domain.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "ecs_domain_certificate" {
   domain_name               = var.ecs_alb_domain_name
   subject_alternative_names = ["*.${var.ecs_alb_domain_name}"]
-  validation_method         = "EMAIL" // TODO: DNS validation currently hangs
+  validation_method         = "DNS"
 
   tags = {
     Name = var.ecs_alb_domain_name
@@ -17,29 +17,28 @@ data "aws_route53_zone" "ecs_domain" {
   private_zone = false
 }
 
-// TODO: Currently hands on DNS validation of certificate
-//resource "aws_route53_record" "cert_validation" {
-//  for_each = {
-//    for ecs in aws_acm_certificate.ecs_domain_certificate.domain_validation_options : ecs.domain_name => {
-//      name   = ecs.resource_record_name
-//      record = ecs.resource_record_value
-//      type   = ecs.resource_record_type
-//    }
-//  }
-//
-//  allow_overwrite = true
-//  name            = each.value.name
-//  records         = [each.value.record]
-//  ttl             = 60
-//  type            = each.value.type
-//  zone_id         = data.aws_route53_zone.ecs_domain.zone_id
-//}
-//
-//resource "aws_acm_certificate_validation" "ecs_domain_certificate_validation" {
-//  certificate_arn         = aws_acm_certificate.ecs_domain_certificate.arn
-//  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-//
-//  timeouts {
-//    create = "10m"
-//  }
-//}
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for ecs in aws_acm_certificate.ecs_domain_certificate.domain_validation_options : ecs.domain_name => {
+      name   = ecs.resource_record_name
+      record = ecs.resource_record_value
+      type   = ecs.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.ecs_domain.zone_id
+}
+
+resource "aws_acm_certificate_validation" "ecs_domain_certificate_validation" {
+  certificate_arn         = aws_acm_certificate.ecs_domain_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+
+  timeouts {
+    create = "10m"
+  }
+}
